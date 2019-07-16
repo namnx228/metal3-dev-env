c7edc65bd96c7c35c15ec25517b6515be70268a5
Nam <namnx228@gmail.com>
2019-07-16 16:34:03 +0300

    Fix error in disable....sh

--- disable_apparmor_driver_libvirtd.sh
+++ disable_apparmor_driver_libvirtd.sh
@@ -1,6 +1,6 @@
 foo="#security_driver = \"selinux\""
 foo2="security_driver = \"apparmor\""
 bar="security_driver = \"none\""
-sudo sed "s/$foo/$bar/g" /etc/libvirt/qemu.conf
-sudo sed "s/$foo2/$bar/g" /etc/libvirt/qemu.conf
+sudo sed -i "s/$foo/$bar/g" /etc/libvirt/qemu.conf
+sudo sed -i "s/$foo2/$bar/g" /etc/libvirt/qemu.conf
 sudo systemctl restart libvirtd 
