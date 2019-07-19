4ee2b64c03f80b2555f0aaf6010087563d07b050
Nam <namnx228@gmail.com>
2019-07-19 15:08:19 +0300

    Fix error in pyenv installation

--- ubuntu_install_requirements.sh
+++ ubuntu_install_requirements.sh
@@ -51,6 +51,7 @@ fi
 
 # Install pyenv
 if [[  $(cat ~/.bashrc) != *PYENV_ROOT* ]]; then
+  git clone git://github.com/yyuu/pyenv.git .pyenv
   echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
   echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
   echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
