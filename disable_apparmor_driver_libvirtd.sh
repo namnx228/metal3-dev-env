93ba783c63cf0f7aa9fe624c31797b3f59bf11c7
namnx228 <namnx228@est.tech>
2019-07-24 17:33:58 +0300

    change permission each time libvirtd is restarted

--- disable_apparmor_driver_libvirtd.sh
+++ disable_apparmor_driver_libvirtd.sh
@@ -4,3 +4,5 @@ bar="security_driver = \"none\""
 sudo sed -i "s/$foo/$bar/g" /etc/libvirt/qemu.conf
 sudo sed -i "s/$foo2/$bar/g" /etc/libvirt/qemu.conf
 sudo systemctl restart libvirtd 
+sudo chmod o+rwx /var/run/libvirt/libvirt-sock
+
